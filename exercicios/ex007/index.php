2b26ceca118e1fb93c17f916f244dfd3f2be7665
MetzkerDeveloper <walerianoadm@gmail.com>
2023-08-02 20:55:28 -0300

    Atualização do código e nova forma de apresentar o código

--- exercicios/ex007/index.php
+++ exercicios/ex007/index.php
@@ -16,8 +16,21 @@
       // 7- Termine o script abaixo, exibindo o nome do mês correspondente à variável $numMes e caso o
       // valor de $numMes esteja fora do intervalo entre 1 e 12, exibir a mensagem “Mês inválido”
 
-      $numMes = 8;
-      switch ($numMes) {
+      $mesArray = [1=>'Janeiro', 
+                   2=>'fevereiro',
+                   3=>'Março',
+                   4=>'Abril',
+                   5=>'Maio',
+                   6=>'Junho',
+                   7=>'Julho',
+                   8=>'Agosto',
+                   9=>'Setembro',
+                   10=>'Outubro',
+                   11=>'Novembro',
+                   12=>'Dezembro' 
+];
+      
+      /*switch ($numMes) {
         case 1:
           $mes = 'Janeiro';
           break;
@@ -59,8 +72,12 @@
           $mes = "Mês Inválido";
           break;
       }
+      */
+    
+      $numMes = 8;
+      $mes = ($numMes <= 12 ) ? $mesArray[$numMes] : " Mês inválido!";
       
-      echo "<p>O mês $numMes refere-se a $mes</p>";
+      echo "<p>O mês $numMes é  $mes</p>";
  
       ?>
     </h4>
