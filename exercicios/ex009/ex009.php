554cb7e62ded0067267d3efaf948cc7136a8d243
MetzkerDeveloper <walerianoadm@gmail.com>
2023-08-06 17:44:04 -0300

    Atualizando o código do exercio proposto em 04/08/2023

--- exercicios/ex009/ex009.php
+++ exercicios/ex009/ex009.php
@@ -3,11 +3,14 @@
 entre 0 e 10. Encontre e mostre o menor elemento e sua posição no vetor 
 */
 
-$arr = array(2,1,9,7,6);
+$arr = [];
+for($i = 0; $i < 5 ; $i++){
+	$arr[]= rand(0,10);
+}
+
 $minArray = min($arr);
 $indice = array_keys($arr, $minArray);
 
 echo "O menor número do array é $minArray e seu indice é $indice[0] "; 
-
 
 ?>
