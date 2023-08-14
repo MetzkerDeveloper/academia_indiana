a5ec9415683a088624ebc7afbb73cbd3353aab96
MetzkerDeveloper <walerianoadm@gmail.com>
2023-08-14 08:36:47 -0300

    Feat: Correção do exercicio de encapsulamento na aula do dia 14/08/2023

--- MODULO2/POO/02_oo/Encapsulamento/exercicio/Pessoa.php
+++ MODULO2/POO/02_oo/Encapsulamento/exercicio/Pessoa.php
@@ -1,70 +1,101 @@
-<?php 
+<?php
 
 class Pessoa
 {
-  private string $nome;
-  private int $idade;
-  private string $corDosOlhos;
-  private string $genero;
-  private float $altura;
-  private float $peso;
+    private string $nome;
+    private int $idade;
+    private string $corDosOlhos;
+    private string $genero;
+    private float $altura;
+    private float $peso;
 
-   public function __construct(string $nome,int $idade, string $corDosOlhos, string $genero, float $altura, float $peso) 
-   {
-    $this->nome = $nome;
-    $this->idade = $idade;
-    $this->setCorDosOlhos($corDosOlhos);
-    $this->setGenero($genero);
-    $this->altura = $altura;
-    $this->peso = $peso;
-  }
-  
-  public function getNome() : string 
-  {
-    return $this->nome;
-  }
-  
-  public function getIdade(): int
-  {
-    return $this->idade;
-  }
-
- public function setCorDosOlhos(string $corDosOlhos):void
- {
-  if($corDosOlhos == "Azul" || $corDosOlhos == "Castanho" || $corDosOlhos == "Verde" || $corDosOlhos == "Preto")
-  {
-    $this->corDosOlhos = $corDosOlhos;   
-  }else{
-    echo "Cor dos olhos inválida" . PHP_EOL;
+    public function __construct(string $nome, int $idade, string $corDosOlhos, string $genero, float $altura, float $peso)
+    {
+        $this->nome = $nome;
+        $this->setIdade($idade);
+        $this->setCorDosOlhos($corDosOlhos);
+        $this->setGenero($genero);
+       $this->setAltura($altura);
+        $this->setPeso($peso);
     }
- }
 
- public function getCorDosOlhos():string
- {
-  return $this->corDosOlhos;
- }
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
 
- public function setGenero(string $genero) : void 
- {
-  if($genero == "Masculino" || $genero == "Feminino")
-  {
-    $this->genero = $genero;
-  }else{
-      echo PHP_EOL . "Genero informado não existe" . PHP_EOL;
-   }
- }
- public function getGenero() : string
- {
-  return $this->genero;
- }
+    public function getIdade(): int
+    {
+        return $this->idade;
+    }
 
- public function getAltura() : float
- {
-  return $this->altura;
- }
- public function getPeso() : float
- {
-  return $this->peso;
- }
+    private function setIdade(int $idade): void 
+    {
+        if (($idade < 0) || ($idade > 100)) {
+            echo "Idade informada é invalida" . PHP_EOL;
+            exit();
+        }
+
+        $this->idade = $idade;
+    }
+
+    private function setCorDosOlhos(string $corDosOlhos): void
+    {
+        $opcoes = ["Azul", "Castanho", "Verde", "Preto", "Rosa"];
+
+        if(in_array($corDosOlhos, $opcoes)) {
+            $this->corDosOlhos = $corDosOlhos;
+            return;
+        }
+
+        echo "Cor dos olhos inválida" . PHP_EOL;
+        exit();
+    }
+
+    public function getCorDosOlhos(): string
+    {
+        return $this->corDosOlhos;
+    }
+
+    private function setGenero(string $genero): void
+    {
+        $opcoes = ["Masculino", "Feminino"];
+
+        if(in_array($genero, $opcoes)) {
+            $this->genero = $genero;
+            return;
+        }
+
+        echo "Genero inválido" . PHP_EOL;
+        exit();
+    }
+
+    public function getGenero(): string
+    {
+        return $this->genero;
+    }
+    private function setAltura(float $altura) : void
+    {
+      if(($altura < 0) || ($altura > 2.20)){
+        echo "Altura inválida";
+        exit();
+      }
+      $this->altura = $altura;
+    }
+    public function getAltura(): float
+    {
+        return $this->altura;
+    }
+    private function setPeso(float $peso) : void
+    {
+      if(($peso < 0) && ($peso > 140) ) {
+        echo "Peso inválido";
+        exit();
+      }
+      $this->peso = $peso;
+    }
+    public function getPeso(): float
+    {
+        return $this->peso;
+    }
 }
-?>
--- MODULO2/POO/02_oo/Encapsulamento/exercicio/index.php
+++ MODULO2/POO/02_oo/Encapsulamento/exercicio/index.php
@@ -3,10 +3,10 @@
 require_once "Pessoa.php";
 
 
-$pessoa = new Pessoa('José', 23,'Castanho','Masculino',1.83, 90.55);
-
+$pessoa = new Pessoa('José', 23,'Azul','Masculino', -2.30, 90.55);
 var_dump($pessoa);
-
+//$pessoa2 = new Pessoa('Wilbert', 23,'Azul','Não informar',1.83, 90.55);
+//var_dump($pessoa2);
 
 
 
